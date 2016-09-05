deeb41ce09ce7991736c585d7c22f546961261e9
joshhsoj1902 <joshhsoj1902@gmail.com>
2016-09-05 15:02:19 -0400

    Made the launch script quicker

--- palindrome/launch_palindrome.sh
+++ palindrome/launch_palindrome.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
-#Digital Ocean doesn't preinstall docer-compose
-apt-get update
-apt-get -y install python-pip;
-pip install docker-compose
+if ! type docker-compose 2>/dev/null; then
+  echo "Docker-compose not installed, installing now"
+  #Digital Ocean doesn't preinstall docer-compose
+  apt-get update
+  apt-get -y install python-pip;
+  pip install docker-compose
+fi
 
 if [ ! -d "palindrome-app" ]; then
   git clone https://github.com/joshhsoj1902/palindrome.git palindrome-app
 else
-  git -C "palindrome-app" fetch 
+  git -C "palindrome-app" fetch --all
+  git -C "palindrome-app" reset --hard origin/master
 fi
 
-docker-compose -f docker-compose.yml up -d
+#If the build fails doing the bower install, there is a good chance the system
+#does not have enough RAM. This would not be an issue if palindrome was on
+#docker-hub
+docker-compose -f docker-compose.yml up --build -d
+docker-compose -f docker-compose.yml scale app=2
--- palindrome/launch_prod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#Digital Ocean doesn't preinstall docker-compose
-apt-get update;
-apt-get -y install python-pip;
-pip install docker-compose;
-
-docker-compose -f docker-compose.yml up -d;
